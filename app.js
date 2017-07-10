7e4a9d83f996eff57d5ee5ccdae8584bc91a874c
Luana Martins <lms7@cin.ufpe.br>
2017-07-10 14:29:09 -0300

    Fix code formatting

--- app.js
+++ app.js
@@ -12,23 +12,19 @@ console.log('Command: ', command);
 //console.log('Process: ', process.argv);
 console.log('Yargs', argv);
 
-if(command === 'add'){
-  var note = notes.addNote(argv.title, argv.body);
-  if (note) {
-    console.log('Note created');
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-  } else {
-    console.log('Note title taken');
-  }
-} else if (command === 'list'){
-  notes.getAll();
-} else if (command === 'read'){
-  notes.getNote(argv.title);
-} else if (command === 'remove'){
-  var noteRemoved = notes.removeNote(argv.title);
-  var message = noteRemoved ? 'Note was removed': 'Note not found';
-  console.log(message);
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+    } else {
+        console.log('Note title taken');
+    }
+
+} else if (command === 'list') {
+    notes.getAll();
+
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
     if (note) {
@@ -40,6 +36,11 @@ if(command === 'add'){
         console.log('Note not found');
     }
 
+} else if (command === 'remove') {
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
+
 } else {
-  console.log('Command not recognized');
+    console.log('Command not recognized');
 }
