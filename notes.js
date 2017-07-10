bf6e6194c88be8c131999007a767d0552e446d99
Luana Martins <lms7@cin.ufpe.br>
2017-07-10 14:07:34 -0300

    Fix code formatting

--- notes.js
+++ notes.js
@@ -4,55 +4,55 @@ const fs = require('fs');
 
 
 var fetchNotes = () => {
-  try {
-    var notesString = fs.readFileSync('data/notes-data.json');
-    return JSON.parse(notesString);
-  } catch(e) {
-    return [];
-  }
+    try {
+        var notesString = fs.readFileSync('data/notes-data.json');
+        return JSON.parse(notesString);
+    } catch(e) {
+        return [];
+    }
 };
 
 var saveNotes = (notes) => {
-  fs.writeFileSync('data/notes-data.json', JSON.stringify(notes));
+    fs.writeFileSync('data/notes-data.json', JSON.stringify(notes));
 };
 
 var addNote = (title, body) => {
-  var notes = fetchNotes();
-  var note = {
-    title,
-    body
-  };
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
 
-  var duplicateNotes = notes.filter((note) => note.title == title);
+    var duplicateNotes = notes.filter((note) => note.title == title);
 
-  if(duplicateNotes.length == 0){
-    notes.push(note);
-    saveNotes(notes);
-    return note;
-  }
+    if(duplicateNotes.length == 0){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 
 var getAll = () => {
-  console.log('Getting all notes.');
+    console.log('Getting all notes.');
 };
 
 var getNote = (title) => {
-  console.log('Getting note', title);
+    console.log('Getting note', title);
 };
 
 var removeNote = (title) => {
-  console.log('Removing note', title);
-  var notes = fetchNotes();
-  var filteredNotes = notes.filter((note) => note.title !== title);
-  saveNotes(filteredNotes);
-  return notes.length !== filteredNotes.length;
+    console.log('Removing note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
-  addNote,
-  getAll,
-  getNote,
-  removeNote
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
 
 
