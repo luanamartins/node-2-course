e8d4987cbc79166541c79fa331cd9991cf7901df
Luana Martins <lms7@cin.ufpe.br>
2017-07-10 15:38:57 -0300

    Add logNote function

--- notes.js
+++ notes.js
@@ -51,11 +51,18 @@ var removeNote = (title) => {
     return notes.length !== filteredNotes.length;
 };
 
+var logNote = (note) => {
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+};
+
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
 
 
