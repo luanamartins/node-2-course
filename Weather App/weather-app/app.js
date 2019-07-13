f840da64f43443d69ba9419bc500dac65d7913c7
Luana Martins <luana.cin@gmail.com>
2019-07-13 17:18:11 -0300

    Add templating and no-found page

--- Weather App/weather-app/app.js
+++ Weather App/weather-app/app.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
+const path = require('path')
 const express = require('express')
+
 const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+const viewsPath = path.join(__dirname, '../templates/views')
+app.set('views', viewsPath)
 
 app.get('', (req, res) => {
     // Provide HTML to render in the browser
@@ -16,16 +24,14 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
 });
-
-// const request = require('request');
-// const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.42331`;
-
-// request({ url: url }, (error, response) => {
-//     const responseBody = JSON.parse(response.body);
-//     console.log(responseBody.daily.data[0].summary + ' It is currently ' +
-//         responseBody.currently.temperature + ' degrees out. There is a ' +
-//         responseBody.currently.precipProbability + '% chance of rain.')
-// });
