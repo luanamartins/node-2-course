108cdad0c049512f2bc22547297dedac8409beda
Luana Martins <luana.cin@gmail.com>
2019-07-13 08:04:45 -0300

    Get temperature

--- Weather App/weather-app/app.js
+++ Weather App/weather-app/app.js
@@ -4,8 +4,8 @@ const request = require('request');
 const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.42331`;
 
 request({ url: url }, (error, response) => {
-    // Parse the response body from JSON string into JavaScript object
-    const data = JSON.parse(response.body);
-    // Will print the current temperature to the console
-    console.log(data.currently.temperature);
+    const responseBody = JSON.parse(response.body);
+    console.log(responseBody.daily.data[0].summary + ' It is currently ' +
+        responseBody.currently.temperature + ' degrees out. There is a ' +
+        responseBody.currently.precipProbability + '% chance of rain.')
 });
