b9106407c8c750c3bfdcd19b58fb40af86cefe8e
Luana Martins <luana.cin@gmail.com>
2019-07-15 20:20:02 -0300

    Add mongoose

--- Task App/app.js
+++ Task App/app.js
@@ -1,24 +1,9 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database!')
-    }
-    const db = client.db(databaseName)
+const mongoose = require('mongoose');
 
-    // Start to interact with the database
-
-    const tasks = [
-        { description: 'Clean the house', completed: true },
-        { description: 'Renew inspection', completed: false }
-    ];
-    db.collection('tasks').insertMany(tasks, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert tasks!');
-        }
-        console.log(result.ops);
-    });
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
+    useNewUrlParser: true,
+    useCreateIndex: true
 });
