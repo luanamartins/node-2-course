5d6746a0f7ddc6a7858c6a9b2f62eeabebd14804
Luana Martins <luana.cin@gmail.com>
2019-07-15 20:22:37 -0300

    Add validator

--- Task App/model/user.js
+++ Task App/model/user.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
-    age: {
-        type: Number
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     }
 });
