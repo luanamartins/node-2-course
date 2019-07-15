d45e9152de2dc2788b2fe0915e5aa3c2233d6fe4
Luana Martins <luana.cin@gmail.com>
2019-07-15 20:44:28 -0300

    Add express

--- Task App/app.js
+++ Task App/app.js
@@ -1,9 +1,21 @@
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
-
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+const User = require('./models/User');
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
-    useNewUrlParser: true,
-    useCreateIndex: true
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body);
+    user.save().then(() => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
 });
