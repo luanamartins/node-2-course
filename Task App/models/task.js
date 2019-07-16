933517b3366b24984c4e8eb915744e97157b1ac1
Luana Martins <luana.cin@gmail.com>
2019-07-16 09:09:06 -0300

    Add task model

---
+++ Task App/models/task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const Task = mongoose.model('Task', {
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}).
