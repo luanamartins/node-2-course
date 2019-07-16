75e851bd5dea0f8ff5afe3af3268d29d582c3a9f
Luana Martins <luana.cin@gmail.com>
2019-07-16 08:50:05 -0300

    Log requests

--- Task App/app.js
+++ Task App/app.js
@@ -6,6 +6,13 @@ const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 const app = express();
 
+// Register the function as middleware for the application
+const loggerMiddleware = (req, res, next) => {
+    console.log('New request to: ' + req.method + ' ' + req.path);
+    next();
+}
+
+app.use(loggerMiddleware);
 app.use(express.json());
 app.use(userRoute);
 
