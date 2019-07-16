037108e5bc1bd0db1d1f87088b233ad32a2e7eb6
Luana Martins <luana.cin@gmail.com>
2019-07-16 09:09:21 -0300

    Add user and task relationship

--- Task App/models/user.js
+++ Task App/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 
+const Task = require('./task');
+
 const schema = mongoose.Schema;
 const userSchema = new Schema({
     name: {
@@ -28,11 +30,23 @@ const userSchema = new Schema({
     }]
  });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
  userSchema.pre('save', async function (next) {
     const user = this;
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+    next();
+});
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
     next();
 });
 
