dbca09de0de5a79b355fea88c11e891342d64505
Luana Martins <luana.cin@gmail.com>
2019-07-16 07:43:53 -0300

    Change promises to async-await function

--- Task App/app.js
+++ Task App/app.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body);
-    user.save().then(() => {
-        res.send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
-    });
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (e) {
+        res.status(500).send();
+    }
 });
 
 app.get('/users/:id', (req, res) => {
